ffd6add3c5cbd5d8bf03f35d1999f14eb66f0aa9
iSyncs <149739497+iSyncs@users.noreply.github.com>
2026-02-24 16:30:03 -0600

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Carlos Baranda on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
